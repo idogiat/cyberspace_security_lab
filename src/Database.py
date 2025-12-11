de86876dddfc95169d7a286aa2a4f5dcc966f5e8
unknown <eidogiat@gmail.com>
2025-12-11 20:16:47 +0200

    Create new databe - setup

---
+++ src/Database.py
@@ -0,0 +1,15 @@
+import sqlite3
+from typing import NamedTuple
+
+
+class Line(NamedTuple):
+    a: int
+    b: str
+
+
+class DB:
+    def __init__(self):
+        pass
+
+    def login(self):
+        pass
