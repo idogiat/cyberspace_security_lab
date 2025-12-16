90d5d72efbf731ebe98c13733b42aa4481dedfd6
ajgraham <ajgraham@AJGRAHAM-PORTEG.il.oracle.com>
2025-12-16 21:23:24 +0200

    SQLite3 basic structure TOTP missing

--- src/Database.py
+++ src/Database.py
@@ -6,8 +6,9 @@ import sqlite3
 
 import bcrypt
 import pyotp
-from argon2 import PasswordHasher
+from argon2 import PasswordHasher, exceptions as exceptions_from_argon2
 from typing import NamedTuple
+
 
 GROUP_SEED = 123456789 # to be corrected
 
@@ -20,7 +21,7 @@ class Line(NamedTuple):
     group_seed: int
     metadata: str
 
-class logs_info(NamedTuple):
+class LogsInfo(NamedTuple):
     timestamp: str
     usernmae: str
     groud_seed: str
@@ -42,9 +43,9 @@ class DB:
         with self.connection:
         
             self.connection.execute("""
-                creates a new table users (
+                CREATE TABLE IF NOT EXISTS users (
                     username TEXT,
-                    password TEXT PRIMARY KEY,
+                    password TEXT,
                     salt TEXT,
                     hash_mode TEXT,
                     group_seed INTEGER,
@@ -52,8 +53,9 @@ class DB:
             )""")
 
     # Creates a new user record in the users table
-    def register(self, username, password, salt, hash_mode, metadata='{}', PEPPER= None):    
+    def register(self, username, password, hash_mode, PEPPER= None, metadata='{}'):    
         with self.connection: 
+            salt = ''
             # TO DO: implement hashes and crypt modes
             match hash_mode:
                 case "TOTP":
@@ -77,16 +79,18 @@ class DB:
                     hashed_password = bcrypt.hashpw(password.encode(),salt).decode()
 
                 case "SHA-256 + SALT":
-                    password = password+salt
+                    # generate a random salt value
+                    salt = os.urandom(16).hex()
+                    password = password + salt
                     # hexdigest to make the password more readable (Hex format)
                     hashed_password = hashlib.sha256(password.encode()).hexdigest()
                 case "SHA-256 + SALT + PEPPER":
                     if PEPPER == None:
-                        print("Requested SHA-256 + SALT + PEPPER mode but PEPPER is empty")
+                        print("Requested SHA-256 + SALT + PEPPER mode but PEPPER was not provided")
                         raise ValueError(PEPPER)
                     else:
                         # generate a random salt value
-                        salt = os.urandom(16)
+                        salt = os.urandom(16).hex()
                         password = password+salt
                         # hexdigest to make the password more readable (Hex format)
                         hashed_password = hashlib.sha256(password.encode()).hexdigest()
@@ -96,48 +100,71 @@ class DB:
 
 
     # log the login attempt to attempts.log
-    def log_attempt(self, logs_info: logs_info, log_file="attempts.log"):
+    def log_attempt(self, logs_info: LogsInfo, log_file="attempts.log"):
         with open(log_file, "a") as f: 
             f.write(json.dumps(logs_info._asdict()) + "\n")
  
+
     # login method
     def login(self, username, password_input, PEPPER=None): 
         users = self.get_user(username)
+        
+        # no user with the input usernmae
+        if not users:
+            return False
+        # check each user with the username=<username>
+        for user in users:
 
-        if users:
-            # check each user with the username=<username>
-            for user in users:
+            match user.hash_mode:
+                case "Argon2":
+                    ph = PasswordHasher()
+                    if PEPPER:
+                        password_input = password_input + PEPPER
+                    try:
+                        # ph.verify throws exception if verification failed
+                        ph.verify(user.password,password_input)
+                        return True
+                    except exceptions_from_argon2.VerifyMismatchError:
+                        continue
+                    
+                case "bcrypt":
+                    if PEPPER:
+                        password_input = password_input + PEPPER
+                    result = bcrypt.checkpw(password_input.encode(), user.password.encode())
+                    if result:
+                        return True
 
-                match user.hash_mode:
-                    case "Argon2":
-                        ps = PasswordHasher()
-                        if PEPPER:
-                            password_input = password_input + PEPPER
-                        
-                        
-                        pass
-                    case "bcrypt":
-                        pass
-                    case "Argon2":
-                        pass
-                    case "Argon2":
-                        pass
-                    case "Argon2":
-                        pass
+                    pass
+                case "SHA-256 + SALT":
+                    password_input = password_input + user.salt
+                    candidate_hash = hashlib.sha256(password_input.encode()).hexdigest()
 
-                if user.password == password_input:
-                    print("user have enter the right password")
-                #TO DO:
+                    if candidate_hash == user.password:
+                        return True
+                    
 
-                # check hash mode
+                    pass
+                case "SHA-256 + SALT + PEPPER":
+                    if not PEPPER:
+                        print("Requested SHA-256 + SALT + PEPPER mode but PEPPER was not provided")
+                    password_input = password_input + user.salt + PEPPER
+                    candidate_hash = hashlib.sha256(password_input.encode()).hexdigest()
 
-                # compare stored hashed password with hash_mode + password
-                pass
+                    if candidate_hash == user.password:
+                        return True
+                    pass
+                case "TOTP":
+                    # TO DO
+                    pass
+            
+            # print("can't verify user with username = " +user.username + "and with password = " + password_input)
+            return False
+            
 
     # get user or users by their username value
     def get_user(self, username):
         with self.connection:
-            fetchResults = self.conn.execute('SELECT username, password_hash, salt, hash_mode, group_seed, metadata FROM users WHERE username=?', (username,))
+            fetchResults = self.connection.execute('SELECT username, password, salt, hash_mode, group_seed, metadata FROM users WHERE username=?', (username,))
             users_records = fetchResults .fetchall()
             
             if users_records:
@@ -145,6 +172,47 @@ class DB:
                 for row in users_records:
                     user = Line(*row)
                     users_list.append(user)
+                    
                 return users_list
             else:
+                print("did not found a user with usernaame = " + username)
                 return None
+            
+    # for testing purposes
+    def delete_all_users(self):
+        with self.connection:
+            self.connection.execute("DELETE FROM users")
+            
+
+
+
+def test_db():
+    # יצירת אובייקט של DB (בהנחה שמימוש המחלקה תקין)
+    db = DB()
+    
+    db.delete_all_users()
+    print("----- Adding users -----")
+    db.register('alice', 'alicepass', 'SHA-256 + SALT')
+    db.register('bob', 'bobpass', 'SHA-256 + SALT + PEPPER',"Our PEPPER")
+    db.register('charlie', 'charliepass', 'bcrypt')
+    db.register('joe', 'joepass', 'Argon2')
+
+    print("----- Login tests -----")
+    print("return true if login succesful, else return false")
+    print("Alice, correct password test: ", db.login('alice', 'alicepass'))  # expect True
+    print("Alice, wrong password login test: ", db.login('alice', 'wrong password'))  # expect false
+    print("Bob, correct password test :", db.login('bob', 'bobpass',"Our PEPPER"))        # expect True
+    print("Bob, wrong password test :", db.login('bob', 'wrong pass',"Our PEPPER"))        #  expect false
+    print("Charlie, correct password test :", db.login('charlie', 'charliepass'))# expect True
+    print("Charlie, wrong password test :", db.login('charlie', 'wrongpass'))# expect False
+    print("joe, correct password test :", db.login('joe', 'joepass'))# expect True
+    print("joe, wrong password test :", db.login('joe', 'wrongpass'))# expect False
+    
+    
+    print("David Not existing user test :", db.login('david', 'nopass'))           # expect false
+
+
+
+
+if __name__ == "__main__":
+    test_db()  # test DB funcitons
